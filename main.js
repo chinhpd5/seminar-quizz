3e4cd5d833f1eb224566f0d810049700f3933494
chinhpd5 <chinhpd5@fpt.edu.vn>
2024-01-27 18:31:26 +0700

    source

---
+++ main.js
@@ -0,0 +1,49 @@
+let Questions = [
+	{
+		question: "Trong Javascript sự kiện Onload thực hiện khi nào?",
+		correct_answer: "Khi website được tải xong",
+		incorrect_answers: [
+			"Khi người dùng mở trang mới",
+			"Khi người dùng nhấp chuột",
+			"Khi người dùng dowload 1 file"
+		]
+	},
+	{
+		question: "var a = 1; var b = ++a * a--; giá trị của b sẽ là",
+		correct_answer: "4",
+		incorrect_answers: [
+			"1",
+			"2",
+			"0"
+		]
+	},
+	{
+		question: "Java script là ngôn ngữ :",
+		correct_answer: "client-side",
+		incorrect_answers: [
+			"Cả client-side và sever-side",
+			"sever-side",
+			"Không thuộc bất cứ dạng nào trong ba dạng trên"
+		]
+	},
+	{
+
+		question: "4 giá trị của boder-radius lần lượt là :",
+		correct_answer: "top-left, top-right, bottom-right, bottom-left",
+		incorrect_answers: [
+			"up, down, front, behind ",
+			"top, bottom, left, right ",
+			"bottom-left, bottom-right, top-right, top-left"
+		]
+	},
+	
+	{
+		question: "Để khai báo mảng trong JS, ta dùng kí hiệu?",
+		correct_answer: "[]",
+		incorrect_answers: [
+			"{}",
+			"()",
+			"||"
+		]
+	}
+];
